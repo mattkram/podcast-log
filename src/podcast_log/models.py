c7ea33edd89167989af72e915de92e6243b981dd
Matt Kramer <matthew.robert.kramer@gmail.com>
2019-01-15 09:19:10 -0600

    Enhance Podcast model and add Episode model

--- src/podcast_log/models.py
+++ src/podcast_log/models.py
@@ -3,3 +3,17 @@ from django.db import models
 
 class Podcast(models.Model):
     title = models.CharField(max_length=200)
+    url = models.URLField()
+    image_url = models.URLField()
+    summary = models.CharField(max_length=200)
+
+
+class Episode(models.Model):
+    podcast = models.ForeignKey(Podcast, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    publication_date = models.DateTimeField()
+    audio_url = models.URLField()
+    image_url = models.URLField()
+    description = models.CharField(max_length=500)
+    duration = models.DurationField()
+    episode_number = models.IntegerField()
