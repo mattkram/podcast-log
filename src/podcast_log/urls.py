efef398323ce38d998a8c4796d4b6e39daa9f3a4
Matt Kramer <matthew.robert.kramer@gmail.com>
2019-01-17 21:12:37 -0600

    Reformat index and add button to add new podcast

--- src/podcast_log/urls.py
+++ src/podcast_log/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("episodes/", views.EpisodeListView.as_view(), name="episodes"),
     path("<int:pk>/", views.PodcastDetailView.as_view(), name="podcast"),
     path("update/", views.update_podcasts, name="update_all"),
+    path("podcast/new/", views.PodcastListView.as_view(), name="add-podcast"),
 ]
