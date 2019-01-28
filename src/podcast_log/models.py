9c1862bbaa8d828a577f94e3686c73230fcb96f1
Matt Kramer <matthew.robert.kramer@gmail.com>
2019-01-28 16:55:53 -0600

    Increase allowable size of podcast summary

--- src/podcast_log/models.py
+++ src/podcast_log/models.py
@@ -8,7 +8,7 @@ class Podcast(models.Model):
     title = models.CharField(max_length=200)
     url = models.URLField()
     image_url = models.URLField()
-    summary = models.CharField(max_length=500)
+    summary = models.CharField(max_length=1000)
     last_refreshed = models.DateTimeField(default=datetime(1, 1, 1))
     refresh_interval = models.DurationField(default=timedelta(hours=1))
     episode_number_pattern = models.CharField(max_length=50, null=True)
