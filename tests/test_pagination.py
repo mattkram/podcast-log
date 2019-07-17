5dc5cc18e8d5b763e5f032530a7943ddaef20132
Matt Kramer <matthew.robert.kramer@gmail.com>
2019-07-16 21:51:40 -0500

    Refactor to use parametrized test, not fixture

--- tests/test_pagination.py
+++ tests/test_pagination.py
@@ -1,19 +1,17 @@
-import functools
 import random
-from typing import Tuple, Any, List
+from functools import partial
+from typing import Any, List, Callable
 
 import pytest
-from _pytest.fixtures import FixtureRequest
 
 from podcast_log.models import Episode
 from podcast_log.pagination import Paginator
 
 EpisodeNumberList = List[int]
-ItemsAndEpisodeNumbers = Tuple[List[Any], EpisodeNumberList]
 
 
 @pytest.fixture()
-def base_episode_numbers() -> EpisodeNumberList:
+def episode_numbers() -> EpisodeNumberList:
     """Construct a list of episode numbers in random order."""
     base_episode_numbers = list(range(25))
     random.shuffle(base_episode_numbers)
@@ -21,39 +19,30 @@ def base_episode_numbers() -> EpisodeNumberList:
 
 
 @pytest.fixture(autouse=True)
-def episodes(base_episode_numbers: EpisodeNumberList) -> None:
-    for num in base_episode_numbers:
+def episodes(episode_numbers: EpisodeNumberList) -> None:
+    for num in episode_numbers:
         episode = Episode(episode_number=num)
         episode.save()
 
 
-@pytest.fixture(params=["unsorted", "increasing", "decreasing"])
-def items_and_expected_episode_numbers(
-    request: FixtureRequest, base_episode_numbers: EpisodeNumberList
-) -> ItemsAndEpisodeNumbers:
-    """A parametrized fixture returning a list of episodes sorted various ways and the episode
-    numbers associated with those episodes."""
-
-    # Use partial to store common arguments to Paginator constructor
-    p = functools.partial(Paginator, Episode.query, items_per_page=10)
-
-    # Develop specialized arguments for different cases
-    paginator = {
-        "unsorted": p(),
-        "increasing": p(sort_column=Episode.episode_number),
-        "decreasing": p(sort_column=Episode.episode_number, reverse_sort=True),
-    }[request.param]
-
-    expected = {
-        "unsorted": base_episode_numbers,
-        "increasing": sorted(base_episode_numbers),
-        "decreasing": sorted(base_episode_numbers, reverse=True),
-    }[request.param][: paginator.items_per_page]
-
-    return paginator.items, expected
-
-
-def test_sorting(items_and_expected_episode_numbers: ItemsAndEpisodeNumbers) -> None:
+@pytest.mark.parametrize(
+    "kwargs,sort_func",
+    [
+        (dict(), lambda x: x),
+        (dict(sort_column=Episode.episode_number), sorted),
+        (
+            dict(sort_column=Episode.episode_number, reverse_sort=True),
+            partial(sorted, reverse=True),
+        ),
+    ],
+    ids=["unsorted", "increasing", "decreasing"],
+)
+def test_sorting(
+    episode_numbers: EpisodeNumberList,
+    kwargs: Any,
+    sort_func: Callable[[EpisodeNumberList], EpisodeNumberList],
+) -> None:
     """The paginator can return items unsorted, or in increasing or decreasing order."""
-    items, expected_episode_numbers = items_and_expected_episode_numbers
-    assert [item.episode_number for item in items] == expected_episode_numbers
+    paginator = Paginator(Episode.query, items_per_page=10, **kwargs)
+    expected_episode_numbers = sort_func(episode_numbers)[: paginator.items_per_page]
+    assert [item.episode_number for item in paginator.items] == expected_episode_numbers
